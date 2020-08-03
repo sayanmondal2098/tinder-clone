f4d628b5bbc54b3d3606afa596e491e74a6a7ad6
sayanmondal2098@gmail.com <sayanmondal2098@gmail.com>
2020-08-04 02:01:51 +0530

    Updates

---
+++ precache-manifest.df522c46d5c63f506bebc66d6ba5d854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b9e84115a94ea695bb3e19b3a6de57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c30a11f8dfd1d0615b",
+    "url": "/static/css/main.e59326f3.chunk.css"
+  },
+  {
+    "revision": "6c47a2ee4b6085b498f1",
+    "url": "/static/js/2.6cc4cdc3.chunk.js"
+  },
+  {
+    "revision": "0b193cffd0567a6da743599aadaf1b7b",
+    "url": "/static/js/2.6cc4cdc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c30a11f8dfd1d0615b",
+    "url": "/static/js/main.f18b3cb3.chunk.js"
+  },
+  {
+    "revision": "67b591f0d0936d5c2a28",
+    "url": "/static/js/runtime-main.772c1402.js"
+  }
+]);
